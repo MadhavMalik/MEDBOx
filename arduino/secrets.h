a8899043bdb9a1d7c3dac62a773603f88560ac66
MadhavMalik <40149943+MadhavMalik@users.noreply.github.com>
2020-08-19 17:03:39 +0530

    Create secrets.h

---
+++ arduino/secrets.h
@@ -0,0 +1,6 @@
+#define SECRET_SSID "inti"                               
+#define SECRET_PASS "12345678"                            
+
+#define SECRET_CH_ID 1061297                                 
+#define SECRET_WRITE_APIKEY "5E88DL24PBL8WOSN"  
+#define SECRET_READ_APIKEY  "DSD6A64DYTXVC3NR"  
