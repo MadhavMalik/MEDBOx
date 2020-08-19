94057fa7ddad830aa9ec42f1dfe78178a9137e7e
MadhavMalik <40149943+MadhavMalik@users.noreply.github.com>
2020-08-19 17:05:53 +0530

    Create alarms.h

---
+++ arduino/alarms.h
@@ -0,0 +1,9 @@
+//#include "setup.h"
+/*int alarm_minute_1 = 00;
+int alarm_hour_1 = 00;
+
+int alarm_minute_2 = 59;
+int alarm_hour_2 = String("0") + String(hour);
+
+int alarm_minute_3 = 00;
+int alarm_hour_3 = 00;*/
