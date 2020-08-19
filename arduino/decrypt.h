307615bebd10a66c587994876bf63bcc17e02f8b
MadhavMalik <40149943+MadhavMalik@users.noreply.github.com>
2020-08-19 17:04:20 +0530

    Create decrypt.h

---
+++ arduino/decrypt.h
@@ -0,0 +1,57 @@
+
+/*#include "setup.h" //med_temp1, med_temp2, hour, minute, compno
+
+void decrypt_data(){
+   
+
+  int half_name = med_temp1;
+  int other_half = med_temp2;
+  String mystr mystr1;
+  
+  char array1[14];
+  char array2[14];
+  
+  mystr += half_name;
+  mystr1 += other_half;
+  
+  mystr.toCharArray(array1,14);
+  mystr1.toCharArray(array2,14);
+  
+  Serial.println(array1[0]);
+  Serial.println(array1[1]);
+  Serial.println(array1[2]);
+  Serial.println(array1[3]);
+  Serial.println(array1[4]);
+  Serial.println(array1[5]);
+  Serial.println(array1[6]);
+  Serial.println(array1[7]);
+  
+  Serial.println("    ");
+  
+  Serial.println(array2[0]);
+  Serial.println(array2[1]);
+  Serial.println(array2[2]);
+  Serial.println(array2[3]);
+  Serial.println(array2[4]);
+  Serial.println(array2[5]);
+  Serial.println(array2[6]);
+  Serial.println(array2[7]);
+  
+  delay(1000);
+
+   
+  
+  }*/
+
+ /* void temp(){
+    long med = 68738380827378;
+    long int med_name = med;
+
+    String name_of_med;
+
+    char array1[14];
+    name_of_med += med_name
+    name_of_med.toCharArray(array1,14);
+    s
+    
+    }*/
